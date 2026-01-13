de1b3a09386c4b4f6b09991062252d048a6f5214
piraterobertstmv <a.moya.valls@icloud.com>
2026-01-13 13:06:11 +0000

    Fix(deps): downgrade to Vite 4.5.0 for stability

--- vite.config.ts
+++ vite.config.ts
@@ -3,24 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/",
-  server: {
-    host: "::",
-    port: 8080,
-  },
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  build: {
-    commonjsOptions: {
-      include: [/node_modules/],
-      transformMixedEsModules: true,
-    },
-  },
-  optimizeDeps: {
-    include: ["react", "react-dom"],
   },
 });
