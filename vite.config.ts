94cafd62c16d7924517f3f161e4d48c6ecdd443f
piraterobertstmv <a.moya.valls@icloud.com>
2026-01-15 20:02:19 +0000

    Fix(build): pin rollup to 3.29.4 and use Node 18

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,13 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    commonjsOptions: {
+      include: [/node_modules/],
+      transformMixedEsModules: true,
+    },
+    rollupOptions: {
+      external: [],
+    },
+  },
 });
